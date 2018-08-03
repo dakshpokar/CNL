29897ebd310a2429025e6e5be192668a28b912bf
Daksh Pokar <dakshpokar@gmail.com>
2018-08-03 22:18:21 +0530

    Complete Chat

--- AssignmentB4/main.py
+++ AssignmentB4/main.py
@@ -37,9 +37,9 @@ class Server:
 			self.sendMessage(msg, uid)
 	def sendMessage(self, msg, uid):
 		self.clients[uid].send(msg)
-		print("##################################################################")
 		
-	def receiveFrom(self, uid):
+	def receiveFrom(self, uid,broad):
+		print("Thread running"+str(uid))
 		while True:
 			msg = self.clients[uid].recv(1024)
 			print("Received from " + "<"+str(self.addr[uid][0]) + "-" + str(uid) +">"+ ": " + msg.decode("utf-8"))
@@ -47,20 +47,43 @@ class Server:
 			if(str(msg.decode("utf-8")) == "exit0"):
 				self.clients[uid].close()
 				break
-				
+			if(broad == 1):
+				msg = "Received from " + "<"+str(self.addr[uid][0]) + "-" + str(uid) +">"+ ": " + msg.decode("utf-8")
+				msg = bytes(msg, "utf-8")
+				for k in self.clients:
+					if(k != uid):
+						self.sendMessage(msg,k)
+					
+			
+	def broadcastSender(self):
+		while True:
+			msg = input()
+			msg = "Received from Server: " + msg
+			msg = bytes(msg, "utf-8")
+			for uid in self.clients:
+				self.sendMessage(msg, uid)
+		
 	def chatP2P(self):
 		os.system("clear")
 		print("Server Side Chat Window")
 		self.showClients()
 		clientID = int(input("Enter ID: "))
 		threadS = threading.Thread(target = self.sender, args = (clientID,))
-		threadR = threading.Thread(target = self.receiveFrom, args = (clientID,))
+		threadR = threading.Thread(target = self.receiveFrom, args = (clientID,0))
 		threadS.start()
 		print("##################################################################")
 		threadR.start()
 		return
 	def chatBroadcast(self):
-		
+		os.system("clear")
+		print("\nList of clients on Server: ")
+		self.showClients()
+		print("##################################################################")
+		threadS = threading.Thread(target = self.broadcastSender, args = ())
+		threadS.start()
+		for uid in self.clients:
+			threadR = threading.Thread(target = self.receiveFrom, args = (uid,1))
+			threadR.start()
 		return
 	def showClients(self):
 		for uid in self.clients:
@@ -74,7 +97,8 @@ class Client:
 		thread.join()	
 		
 	def run(self):
-		ip = str(input("Enter the IP of the Server: "))
+		#ip = str(input("Enter the IP of the Server: "))
+		ip = "127.0.0.1"
 		port = int(input("Enter the port number of Server: "))
 		self.s = socket.socket()
 		self.s.connect((ip, port))
@@ -85,7 +109,7 @@ class Client:
 		os.system("clear")
 		print("Peer-2-Peer Chat Window Started")
 		threadS = threading.Thread(target = self.sendMessage, args = ())
-		threadR = threading.Thread(target = self.receiveFrom, args = ())
+		threadR = threading.Thread(target = self.receiveFrom, args = (0,))
 		threadS.start()
 		print("##################################################################")
 		threadR.start()
@@ -94,18 +118,28 @@ class Client:
 	
 		return
 	def chatBroadcast(self):
-		
+		os.system("clear")
+		print("MultiUser Chat Window Started")
+		threadS = threading.Thread(target = self.sendMessage, args = ())
+		threadR = threading.Thread(target = self.receiveFrom, args = (1,))
+		threadS.start()
+		print("##################################################################")
+		threadR.start()
 		return
 	def sendMessage(self):
 		while True:
 			msg = bytes(input(), "utf-8")
 			self.s.send(msg)
 			print("##################################################################")
-	def receiveFrom(self):
+	def receiveFrom(self,broad):
 		while True:
 			msg = self.s.recv(1024)
-			print("Received from Server: " + msg.decode("utf-8"))
-			print("##################################################################")
+			if(broad == 1):
+				print(msg.decode("utf-8"))
+				print("##################################################################")
+			else:
+				print("Received from Server: " + msg.decode("utf-8"))
+				print("##################################################################")
 	
 class Chat:
 	server = None
@@ -165,6 +199,7 @@ class Chat:
 				self.client.chatP2PClient()
 			elif(choice == 3):
 				self.client.chatBroadcast()
+				break
 			else:
 				print("Invalid Choice")
 			print("#################################")
